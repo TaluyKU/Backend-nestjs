0f4f08601b651c1e0f9710a89a11be1115c4607e
tattepr <mild.4112@gmail.com>
2024-03-29 19:07:50 +0700

    set CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'],
+    cors: true,
   });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.setGlobalPrefix('api');
